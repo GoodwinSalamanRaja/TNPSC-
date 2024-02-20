8a11ded8930cd9ca02ec72317ad5853a0da04e3c
GoodwinSalamanRaja <160569021+GoodwinSalamanRaja@users.noreply.github.com>
2024-02-20 11:02:04 +0530

    myquasarproject_1

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,247 @@
+<template>
+  <div>
+    <q-layout view="lHh lpr lFf">
+      <q-header elevated style="background-color: black;height: 70px;" class="flex glossy">
+        <q-toolbar>
+          <!-- <q-btn flat round dense icon="menu" class="desktop-only" /> -->
+          <q-avatar class="q-ml-md">
+            <img
+              src="https://static.gameloop.com/img/1326a436dede55f1adce87c24b8e4930.png?imageMogr2/thumbnail/172.8x172.8/format/webp">
+          </q-avatar>
+          <q-toolbar-title class="q-mt-sm">
+            <img src="https://tamil.examsdaily.in/wp-content/uploads/2022/01/examsdaily-logo-png-300x82-1.png" height="40"
+              width="160">
+          </q-toolbar-title>
+          <q-tabs v-model="tab" class="flex flex-between desktop-only q-mr-xl">
+            <q-tab name="home" label="Home" />
+            <q-tab name="about us" label="About Us" />
+            <q-tab name="testimonals" label="Limitations" />
+            <q-tab name="contact us" label="Contact Us" />
+          </q-tabs>
+          <q-btn flat round dense icon="menu" aria-label="Menu" @click="toggleRightDrawer" class="q-mr-sm mobile-only" />
+        </q-toolbar>
+
+
+      </q-header>
+
+      <q-page-container>
+        <RouterView />
+      </q-page-container>
+      <footer style="margin-top: 100px;background-color: black;" id="footer" class="q-pa-xl glossy aos-init aos-animate"
+        data-aos="fade-up">
+        <div class="container aos-init aos-animate" data-aos="fade-up">
+          <div>
+            <hr class="bg-warning no-border hr">
+            <h1 class="text-center text-white aos-init aos-animate no-margin" data-aos="fade-up"
+              style="font-weight: bolder;font-size: 6vw;line-height: 7vw;">Contact Us</h1>
+          </div>
+          <hr class="q-mt-xl no-border" style="height: 2px;background-color: rgb(162, 162, 162);">
+          <div class="row q-mt-xl text-white q-pt-xl q-pb-xl q-gutter-md aos-init aos-animate" data-aos="fade-up">
+            <div class="col-12 col-md q-mt-lg flex items-center aos-init aos-animate" data-aos="fade-up">
+              <img src="https://examsdaily.in/wp-content/uploads/2020/12/examsdaily-new-png-png.png" height="90"
+                width="360" class="footimg">
+              <p style="color: rgb(162,162,162);font-weight: bold;line-height: 30px;">The automated
+                process starts as soon as your clothes go into the machine.Discover tools and techniques to engage
+                effectively with young people.</p>
+            </div>
+            <div class="col-12 col-md q-mt-lg flex column items-center aos-init aos-animate footmenu" data-aos="fade-up">
+              <div>
+                <h3><b>Menu</b></h3>
+                <div class="q-mt-lg" style="line-height: 30px;font-weight: bold;">
+                  <a href="#" class="foot">Home</a><br>
+                  <a href="#" class="foot">About Us</a><br>
+                  <a href="#" class="foot">Limitations</a><br>
+                  <a href="#" class="foot">Gallery</a><br>
+                  <a href="#" class="foot">Contact Us</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md q-mt-lg aos-init aos-animate" data-aos="fade-up">
+              <h3><b>Support</b></h3>
+              <div class="q-mt-lg" style="line-height: 30px;font-weight: bold;">
+                <a href="#" class="foot">Telecommunication</a><br>
+                <a href="#" class="foot">Programming</a><br>
+                <a href="#" class="foot">Term and Conditions</a><br>
+                <a href="#" class="foot">Privacy Policy</a><br>
+                <a href="#" class="foot">Community</a>
+              </div>
+            </div>
+            <div class="col-12 col-md q-mt-lg aos-init aos-animate" data-aos="fade-up" style="font-weight: 500;">
+              <h3><b>Contact</b></h3>
+              <div class="flex q-gutter-sm no-wrap">
+                <img src="https://themewagon.github.io/course/images/placeholder.svg" height="25px" width="25px">
+                <span style="word-spacing: 1px;text-wrap: nowrap;">Annai theresa Street, Kalavasal, Madurai-16.</span>
+              </div>
+              <div class="flex q-gutter-sm q-mt-md">
+                <img src="https://themewagon.github.io/course/images/smartphone.svg" height="25px" width="25px">
+                <num class="q-mt-sm">8055224403</num>
+              </div>
+              <a href="https://mail.google.com/chat/u/0/?hl=en#chat/dm/upUCg0AAAAE" target="_blank"
+                class="flex q-gutter-sm q-mt-md mail" style="color: white;text-decoration: none;">
+                <img src="https://themewagon.github.io/course/images/envelope.svg" height="25px" width="25px">
+                <span class="q-mt-sm">contact@edextech.io</span>
+              </a>
+            </div>
+          </div>
+          <hr class="q-mt-xl no-border" style="height: 1px; background-color:  rgb(162, 162, 162);">
+          <div class="row">
+            <div class="col col-12 col-sm-7 text-white q-mt-lg">
+              <p style="font-weight: bold;font-size: medium;word-spacing: 2px;">Copyright ©2024 All rights reserved | This
+                template is made with <span>♥</span></p>
+            </div>
+            <div class="col col-12 col-sm-5 q-mt-lg">
+              <div class="flex justify-end q-gutter-xl foottemplate">
+                <a href="#" class="footicon">
+                  <i class="bi bi-twitter"></i>
+                </a>
+                <a href="#" class="footicon">
+                  <i class="bi bi-facebook"></i>
+                </a>
+                <a href="#" class="footicon">
+                  <i class="bi bi-instagram"></i>
+                </a>
+                <a href="#" class="footicon">
+                  <i class="bi bi-linkedin"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <q-drawer v-model="rightDrawerOpen" side="right" bordered>
+        <q-list>
+          <q-item-label header>
+            <q-input v-model="search" filled type="search" label="Search..">
+              <template v-slot:append>
+                <q-icon name="search" style="margin-bottom: 25px;" />
+              </template>
+            </q-input>
+          </q-item-label>
+
+          <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+        </q-list>
+      </q-drawer>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const linksList = [
+  {
+    title: 'Home',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'About Us',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Limitations',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Contact Us',
+    caption: '@quasarframework',
+    icon: 'record_voice_over',
+    link: 'https://twitter.quasar.dev'
+  },
+  // {
+  //   title: 'Facebook',
+  //   caption: '@QuasarFramework',
+  //   icon: 'public',
+  //   link: 'https://facebook.quasar.dev'
+  // },
+  // {
+  //   title: 'Quasar Awesome',
+  //   caption: 'Community Quasar projects',
+  //   icon: 'favorite',
+  //   link: 'https://awesome.quasar.dev'
+  // }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup() {
+    const rightDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  },
+  mounted() {
+    AOS.init({
+      duration: 1000,
+      offset: 300,
+      // easing: 'ease',
+      // once: true
+    })
+  }
+})
+</script>
+<style scoped>
+.foot {
+  color: rgb(162, 162, 162);
+  text-decoration: none;
+}
+
+.foot:hover {
+  color: rgb(255, 182, 6);
+}
+
+.footicon {
+  color: white;
+  font-size: large;
+}
+
+.footicon :hover {
+  color: rgb(255, 182, 6);
+  transition: transform 2s;
+  transform: scale(1.5);
+}
+
+.mail :hover {
+  text-decoration: underline;
+}
+
+@media screen and (max-width:1023px) {
+  .footmenu {
+    align-items: start;
+  }
+}
+
+@media screen and (max-width:600px) {
+  .foottemplate {
+    justify-content: flex-start;
+  }
+}
+
+@media screen and (max-width:1235px) {
+  .footimg {
+    width: 220px;
+  }
+}
+
+.hr {
+  width: 9vw;
+  height: 0.5vw;
+}</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,399 @@
+<template>
+  <q-page class="flex flex-center">
+    <section class="q-pa-xl div1">
+      <div class="row flex justify-center" style="column-gap: 90px;overflow: hidden;">
+        <div class="col-12 q-pb-xl col-lg-6 aos-init aos-animate text-white" data-aos="fade-up">
+          <h2 class="homeh"><b>Tnpsc Coaching center in Madurai</b></h2>
+          <p style="font-weight: 500;font-size: large;">Tnpsc Coaching Center in Madurai, Tamil Nadu Public Service Commission has had a
+            glorious past of 75 years.
+            During 1923, the Lee Commission had recommended setting up of a Central Public Service Commission in India,
+            but
+            it did not give much thought to the need of establishing similar.</p>
+          <p style="font-weight: 500;font-size: large;">National Institute, the best <b>TNPSC Coaching Centre in Madurai</b> for all Group
+            exams is very glad to
+            announce the aspirants that We National Institute is providing exclusive coaching for TNPSC bank Exam Group
+            Exams with highly-skilled, efficient and experienced faculties. It’s a well-known fact that We National
+            Institute is the <b>best training institute for TNPSC</b> all sort of exams conducted by the TNPSC not only in
+            Madurai all over the state with proven records and the students of We National Institute are directly working
+            under government of Tamil Nadu on the respective departments.</p>
+        </div>
+        <div class="col-12 col-lg-5 q-mt-xl flex self-center aos-init aos-animate" data-aos="fade-up">
+          <img
+            src="https://nationalinstitutemadurai.in/courses/wp-content/uploads/2022/01/national-inistitute-madurai.png"
+            style="width: 100%;height: auto;">
+        </div>
+      </div>
+    </section>
+    <section class="aos-init aos-animate" data-aos="fade-up" style="margin-top: 90px;background-color: rgb(249,249,249);">
+      <div class="q-mt-xl q-pt-xl">
+        <hr class="bg-warning no-border hr">
+        <h1 class="text-center aos-init aos-animate no-margin" data-aos="fade-up"
+          style="font-weight: bolder;font-size: 6vw;line-height: 7vw;">About Us</h1>
+      </div>
+      <div class="row q-pa-xl aos-init aos-animate" data-aos="fade-up">
+        <div class="col col-12 col-lg aos-init aos-animate" data-aos="fade-right">
+          <img
+            src="https://st2.depositphotos.com/3822073/7657/i/450/depositphotos_76579133-stock-photo-high-angle-view-of-an.jpg"
+            height="100%" width="100%">
+        </div>
+        <div class="col col-12 col-lg q-pa-sm aos-init aos-animate" data-aos="fade-right"
+          style="font-size: large;display: flex;flex-direction: column; gap: 40px;">
+          <div>
+            <h2 class="abouth"><b>WHY CHOOSE EXAMSDAILY IN MADURAI <span style="color: rgb(242,4,85);">FOR TNPSC COACHING?</span></b></h2>
+            <p style="font-weight: 500;">Tamil Nadu Public Service Commission exam is the name of the exam through which recruitments to government
+              jobs in Tamil Nadu are made. The syllabus of this exam is according to the degree and qualification of the
+              candidate. Some of the papers are also specific to the posts to which recruitments are made. Tnpsc Coaching
+              Center in Madurai.</p>
+          </div>
+          <div class="row q-pa-md shadow-2 aos-init aos-animate div2" data-aos="fade-up" v-for="i in datas" :key="i"
+            style="background-color: rgb(255,255,255);border-radius: 30px;">
+            <div class="col-12 col-lg-3 q-mt-xl">
+              <img :src="i.img" height="140px">
+            </div>
+            <div class="col-12 col-lg-9">
+              <h5><b>{{ i.title }}</b></h5>
+              <p style="font-weight: 500;">{{ i.para }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="row text-white q-pa-md q-ma-sm aos-init aos-animate" data-aos="fade-up"
+      style="gap: 20px;background-color: rgb(242,4,85);margin-top: 100px;border-radius: 30px;width: 100%;">
+      <div class="col col-12 col-sm-6 col-lg">
+        <div class="flex no-wrap" style="column-gap: 20px;">
+          <div class="q-mt-xl">
+            <img src="https://nationalinstitutemadurai.in/courses/wp-content/uploads/2021/11/funfacts-icon4.png"
+              height="140px">
+          </div>
+          <div>
+            <h3><b>021+</b></h3>
+            <b class="q-ml-sm">Professors</b>
+          </div>
+        </div>
+      </div>
+      <div class="col col-12 col-sm-5 col-lg">
+        <div class="flex no-wrap" style="column-gap: 20px;">
+          <div class="q-mt-xl">
+            <img src="https://nationalinstitutemadurai.in/courses/wp-content/uploads/2021/11/funfacts-icon3.png"
+              height="140px">
+          </div>
+          <div class="q-mt-xs">
+            <h3><b>03</b></h3>
+            <b class="q-ml-sm">Library</b>
+          </div>
+        </div>
+      </div>
+      <div class="col col-12 col-sm-6 col-lg">
+        <div class="flex no-wrap" style="column-gap: 20px;">
+          <div class="q-mt-xl">
+            <img src="https://nationalinstitutemadurai.in/courses/wp-content/uploads/2021/11/funfacts-icon2.png"
+              height="140px">
+          </div>
+          <div>
+            <h3><b>0250</b></h3>
+            <b class="q-ml-sm">No of Students</b>
+          </div>
+        </div>
+      </div>
+      <div class="col col-12 col-sm-5 col-lg">
+        <div class="flex no-wrap" style="column-gap: 20px;">
+          <div class="q-mt-xl">
+            <img src="https://nationalinstitutemadurai.in/courses/wp-content/uploads/2021/11/funfacts-icon1.png"
+              height="140px">
+          </div>
+          <div>
+            <h3><b>0120</b></h3>
+            <b class="q-ml-sm">Passout Students</b>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="q-pa-md aos-init aos-animate div3" data-aos="fade-up" style="margin-top: 140px;">
+      <div class="row q-pa-sm aos-init aos-animate" data-aos="fade-up">
+        <div class="col col-12 col-lg-6 text-warning flex justify-center items-center aos-init aos-animate"
+          data-aos="fade-up">
+          <div style="background-color: rgba(0,0,0,0.5);" class="q-pa-lg aos-init aos-animate" data-aos="fade-right">
+            <ul>
+              <h2 class="formh"><b>
+                  <li>Interested in discussing?</li>
+                </b></h2>
+              <h2 class="formh"><b>
+                  <li>Request a call back</li>
+                </b></h2>
+              <h2 class="formh"><b>
+                  <li>Book Your Demo!!</li>
+                </b></h2>
+              <h2 class="formh"><b>
+                  <li>Get A Free Service Now!</li>
+                </b></h2>
+            </ul>
+          </div>
+        </div>
+        <div class="col col-12 col-lg-6 text-warning q-pb-xl flex justify-center items-center aos-init aos-animate form"
+          data-aos="fade-up" style="height: 92vh;">
+          <div class="aos-init aos-animate formpad" data-aos="fade-left" style="background-color: rgba(0,0,0,0.5);">
+            <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+              <q-input filled type="text" v-model="name" label="Enter Your Name" hint="Name and surname" lazy-rules
+                :rules="[val => val && val.length > 0 || 'Please type your Name']" ref="customname"
+                :color="inputLineColor" />
+              <q-input filled type="email" v-model="email" label="Enter Your Email" hint="Name and surname" lazy-rules
+                :rules="[val => val && val.length > 0 || 'Please type your Email']" ref="customemail"
+                :color="inputLineColor" />
+              <q-input filled type="number" v-model="number" label="Enter Your Phone NO" lazy-rules :rules="[
+                val => val && val.length == 10 || 'Please type your Number'
+              ]" ref="customno" :color="inputLineColor" />
+
+              <q-toggle color="yellow" v-model="accept" label="I accept the license and terms" />
+
+              <div class="text-center q-mt-lg">
+                <q-btn label="Submit" type="submit" color="positive" />
+                <q-btn label="Reset" type="reset" color="accent" class="q-ml-lg" />
+              </div>
+            </q-form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="q-mt-xl q-pa-xl" style="background-color: rgb(0,0,0);width: 100%;">
+      <div class="q-mt-xl">
+        <hr class="bg-warning no-border hr">
+        <h1 class="text-center aos-init aos-animate no-margin text-white" data-aos="fade-up"
+          style="font-weight: bolder;font-size: 6vw;line-height: 7vw;">Limitations</h1>
+      </div>
+      <div class="row q-pb-xl text-white aos-init aos-animate limc" data-aos="fade-up">
+        <div class="col-12 col-lg-7 aos-init aos-animate" data-aos="fade-right">
+          <h1 class="limh"><b>Eligible Criteria For Tnpsc Exam</b></h1>
+          <p class="limp">(Tamil Nadu Public Service Commission) formed in 1923 as the successor of Madras Service Commission as a constitutional body responsible for conducting various exams for recruitment for various posts in to Tamil Nadu government.</p>
+          <div class="row q-mt-xl q-gutter-md" data-aos="fade-up">
+            <div class="col-12 col-lg-5 q-pa-md flex no-wrap rounded-borders limcol" style="column-gap: 20px;">
+              <i class="bi bi-check-circle-fill" style="font-size: 40px;color: rgb(242,4,85);"></i>
+              <p class="limp"><b>Tnpsc Group I age limit 21 to 37 Any Degree</b></p>
+            </div>
+            <div class="col-12 col-lg-5 q-pa-md flex no-wrap rounded-borders" style="background-color: rgb(51,51,51);column-gap: 20px;">
+              <i class="bi bi-check-circle-fill" style="font-size: 40px;color: rgb(242,4,85);"></i>
+              <p class="limp"><b>Tnpsc Group II No age limit Any Degree</b></p>
+            </div>
+            <div class="col-12 col-lg-5 q-pa-md flex no-wrap rounded-borders" style="background-color: rgb(51,51,51);column-gap: 20px;">
+              <i class="bi bi-check-circle-fill" style="font-size: 40px;color: rgb(242,4,85);"></i>
+              <p class="limp"><b>Tnpsc Group II A no age limit Any Degree</b></p>
+            </div>
+            <div class="col-12 col-lg-5 q-pa-md flex no-wrap rounded-borders" style="background-color: rgb(51,51,51);column-gap: 20px;">
+              <i class="bi bi-check-circle-fill" style="font-size: 40px;color: rgb(242,4,85);"></i>
+              <p class="limp"><b>Tnpsc group IV no age limit SSLC</b></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-5 q-mt-xl q-pt-xl aos-init aos-animate" data-aos="fade-right">
+          <img src="https://nationalinstitutemadurai.in/courses/wp-content/uploads/2021/12/nationalinstitutemadurai.png" width="100%">
+        </div>
+      </div>
+    </section>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+export default defineComponent({
+
+  name: 'IndexPage',
+  data: function () {
+    return {
+      inputLineColor: 'yellow',
+      name: "",
+      email: "",
+      number: "",
+      datas: [
+        {
+          img: "https://nationalinstitutemadurai.in/courses/wp-content/uploads/2021/11/funfacts-icon3.png",
+          title: "HIGHLY QUALIFIED FACULTY",
+          para: "Experts from the Industry will ensure you are well-trained & Industry-ready. This core team awaits here to guide you step by step towards overall excellence that automatically leads to success."
+        },
+        {
+          img: "https://nationalinstitutemadurai.in/courses/wp-content/uploads/2021/11/funfacts-icon2.png",
+          title: "PROVED PERFORMANCE",
+          para: "While we have many awards & accolades to our credit, the outstanding success of our students are our proudest testimony. No matter from which university or background you come from, you are bound to feel confident about performing well."
+        },
+        {
+          img: "https://nationalinstitutemadurai.in/courses/wp-content/uploads/2021/11/funfacts-icon4.png",
+          title: "STUDY MATERIAL",
+          para: "High quality support materials to help you prepare better. All our teachers, plus more than 10 course content researchers are constantly at work on our study materials."
+        }
+      ]
+    }
+  },
+  mounted() {
+    AOS.init({
+      duration: 1000,
+      offset: 300,
+      // easing: 'ease',
+      // once: true
+    })
+    this.$refs.customname.$el.querySelector('.q-field__label').style.color = 'rgb(242,192,55)';
+    this.$refs.customemail.$el.querySelector('.q-field__label').style.color = 'rgb(242,192,55)';
+    this.$refs.customno.$el.querySelector('.q-field__label').style.color = 'rgb(242,192,55)';
+    this.$refs.customname.$el.querySelector('.q-field__control input').style.color = 'white'
+    this.$refs.customemail.$el.querySelector('.q-field__control input').style.color = 'white'
+    this.$refs.customno.$el.querySelector('.q-field__control input').style.color = 'white'
+    const labelName = this.$refs.customname.$el.querySelector('.q-field__label');
+    const inputName = this.$refs.customname.$el.querySelector('.q-field__control input');
+    inputName.style.fontSize = '17px';
+    labelName.style.fontSize = '20px';
+    const labelEmail = this.$refs.customemail.$el.querySelector('.q-field__label');
+    const inputEmail = this.$refs.customemail.$el.querySelector('.q-field__control input');
+    inputEmail.style.fontSize = '17px';
+    labelEmail.style.fontSize = '20px';
+    const labelNo = this.$refs.customno.$el.querySelector('.q-field__label');
+    const inputNo = this.$refs.customno.$el.querySelector('.q-field__control input');
+    inputNo.style.fontSize = '17px';
+    labelNo.style.fontSize = '20px';
+
+  },
+  setup() {
+    const $q = useQuasar()
+
+    const name = ref(null)
+    const number = ref(null)
+    const email = ref(null)
+    const accept = ref(false)
+
+    return {
+      name,
+      email,
+      accept,
+      number,
+
+      onSubmit() {
+        if (accept.value !== true) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'You need to accept the license and terms first'
+          })
+        }
+        else {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Submitted'
+          })
+        }
+      },
+
+      onReset() {
+        name.value = null
+        email.value = null
+        number.value = null
+        accept.value = false
+      }
+    }
+  }
+})
+</script>
+<style scoped>
+* {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  /* font-weight: 500; */
+  word-spacing: 2px;
+}
+
+.div1 {
+  background-color: rgb(27, 27, 27);
+}
+
+/* .div2 :hover {
+  background-color: rgb(242, 4, 85);
+  border-radius: 30px;
+  color: white;
+} */
+
+.div3 {
+  background-image: url("../assets/aws.png");
+  height: 92vh;
+  width: 100%;
+  background-size: cover;
+  overflow: hidden;
+}
+
+.formh {
+  font-size: 3vw;
+}
+.limh{
+  font-size: 3.5vw;
+  line-height: 7vw;
+}
+.limp{
+  font-size: 1.2vw;
+}
+.limc{
+  margin-top: 60px;
+}
+.abouth{
+  font-size: 2.8vw;
+  line-height: 3.5vw;
+}
+.homeh{
+  font-size: 4vw;
+  line-height: 4.2vw;
+}
+.limcol{
+  background-color: rgb(51,51,51);
+}
+/* .limcol :hover{
+  background-color:  rgb(242,4,85);;
+} */
+@media screen and (max-width: 1072px) {
+  .div3 {
+    height: 100%;
+  }
+
+  .formh {
+    font-size: 5vw;
+    line-height: 7vw;
+  }
+
+  .form {
+    margin-top: 60px;
+  }
+  .limh{
+    font-size: 5.8vw;
+  }
+  .limp{
+    font-size: 4.3vw;
+  }
+  .limc{
+    margin-top: 40px;
+  }
+  .abouth{
+    font-size: 5vw;
+    line-height: 7vw;
+  }
+  .homeh{
+  font-size: 8.5vw;
+  line-height: 10vw;
+  }
+
+}
+
+.formpad {
+  padding: 90px;
+}
+
+@media screen and (max-width: 455px) {
+  .formpad {
+    padding: 40px;
+  }
+}
+
+.hr {
+  width: 9vw;
+  height: 0.5vw;
+}
+</style>
